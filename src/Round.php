567299d42583c458fd7775a8a8936ad56a863906
Michał Mleczko <kontakt@michalmleczko.waw.pl>
2016-11-28 22:03:42 +0100

    Add round type constants

---
+++ src/Round.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace mleczakm\RSTMoney;
+
+/**
+ * Created by PhpStorm.
+ * User: mleczakm
+ * Date: 28.11.16
+ * Time: 21:37
+ */
+class Round
+{
+    const UP = 'up';
+    const DOWN = 'down';
+}
